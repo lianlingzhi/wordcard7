ed9a515140b7fc9d5e87dfb623f271fdbef6395d
连凌志 <lpkufwfn@sina.com>
2013-04-04 09:23:02 +0800

    word
    First time

---
+++ src/word.java
@@ -0,0 +1,38 @@
+
+public class word {
+	private String word;
+    private String meaning;
+    private String voice;
+    private String sentence;
+    private String phrase;
+	public String getWord() {
+		return word;
+	}
+	public void setWord(String word) {
+		this.word = word;
+	}
+	public String getMeaning() {
+		return meaning;
+	}
+	public void setMeaning(String meaning) {
+		this.meaning = meaning;
+	}
+	public String getVoice() {
+		return voice;
+	}
+	public void setVoice(String voice) {
+		this.voice = voice;
+	}
+	public String getSentence() {
+		return sentence;
+	}
+	public void setSentence(String sentence) {
+		this.sentence = sentence;
+	}
+	public String getPhrase() {
+		return phrase;
+	}
+	public void setPhrase(String phrase) {
+		this.phrase = phrase;
+	}
+}
